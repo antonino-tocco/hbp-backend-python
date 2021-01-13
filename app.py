dc5b1ce41fb6f30550897e28268f90367dee65a8
antonino-tocco <entonytocco@gmail.com>
2021-01-13 20:46:28 +0100

    fix for config

--- app.py
+++ app.py
@@ -54,9 +54,8 @@ try:
     print(f'*******************')
     print(f'RUN APP')
     print(f'*******************')
-    app.run(host='0.0.0.0', port=5000)
-    #http_server = WSGIServer(('', 5000), app)
-    #http_server.serve_forever()
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
 
 except Exception as ex:
     print(f'*******************')
