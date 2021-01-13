1e168ecaad107cee9ce53e04c9c6a70c8c2fa0dd
antonino-tocco <entonytocco@gmail.com>
2021-01-13 22:45:38 +0100

    fix for search and download url

--- helpers/providers/neuro_morpho.py
+++ helpers/providers/neuro_morpho.py
@@ -138,7 +138,7 @@ class NeuroMorphoProvider(Provider):
                 'icon': dataset['png_url'],
                 'link': dataset['_links']['self']['href'],
                 'original_format': dataset['original_format'],
-                'download_original_format': f"http://neuromorpho.org/dableFiles/{dataset['archive']}/Source-Version/{dataset['neuron_name']}.{original_format_ext}",
+                'download_original_format': f"http://neuromorpho.org/dableFiles/{dataset['archive'].lower()}/Source-Version/{dataset['neuron_name']}.{original_format_ext}",
                 'page_link': f"http://neuromorpho.org/neuron_info.jsp?neuron_name={dataset['neuron_name']}",
                 'protocol': dataset['protocol'],
                 'morphologies': dataset['attributes'],
