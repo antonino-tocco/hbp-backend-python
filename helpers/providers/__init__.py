35d7ca357b5a7db0a0a9787475e294c08d36cf6e
antonino-tocco <entonytocco@gmail.com>
2021-01-24 23:18:25 +0100

    fix for Dockerfile

--- helpers/providers/__init__.py
+++ helpers/providers/__init__.py
@@ -1,6 +1,5 @@
 from .neuro_morpho import NeuroMorphoProvider
 from .model_db import ModelDbProvider
-from .knowledge import KnowledgeProvider
 
 enabled_dataset_providers = [NeuroMorphoProvider()]
-enabled_model_providers = [KnowledgeProvider(), ModelDbProvider()]
+enabled_model_providers = [ModelDbProvider()]
