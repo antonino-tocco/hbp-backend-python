25730eb1c44e96d2bedc658fc5a4bf906435bb0c
antonino-tocco <entonytocco@gmail.com>
2021-02-08 20:03:59 +0100

    fix for elastic search

--- helpers/storage/elastic.py
+++ helpers/storage/elastic.py
@@ -52,29 +52,34 @@ class ElasticStorage(Storage):
                     s = s.filter('term', **{'type.keyword': data_type})
                 if secondary_region is not None:
                     if isinstance(secondary_region, str):
-                        s = s.filter('term', **{'secondary_region.keyword': secondary_region})
-                    else:
-                        s = s.filter('terms', **{'secondary_region': secondary_region})
+                        if secondary_region != '':
+                            s = s.filter('term', **{'secondary_region.keyword': secondary_region})
+                    elif secondary_region:
+                        s = s.filter('terms', **{'secondary_region.keyword': secondary_region})
                 if cell_type is not None:
                     if isinstance(cell_type, str):
-                        s = s.filter('term', **{'cell_type.keyword': cell_type})
-                    else:
-                        s = s.filter('terms', **{'cell_type': cell_type})
+                        if cell_type != '':
+                            s = s.filter('term', **{'cell_type.keyword': cell_type})
+                    elif cell_type:
+                        s = s.filter('terms', **{'cell_type.keyword': cell_type})
                 if species is not None:
                     if isinstance(species, str):
-                        s = s.filter('term', **{'species.keyword': species})
-                    else:
-                        s = s.filter('terms', **{'species': species})
+                        if species != '':
+                            s = s.filter('term', **{'species.keyword': species})
+                    elif species:
+                        s = s.filter('terms', **{'species.keyword': species})
                 if channels is not None:
                     if isinstance(channels, str):
-                        s = s.filter('term', **{'channels.keyword': channels})
-                    else:
-                        s = s.filter('terms', **{'channels': channels})
+                        if channels != '':
+                            s = s.filter('term', **{'channels.keyword': channels})
+                    elif channels:
+                        s = s.filter('terms', **{'channels.keyword': channels})
                 if receptors is not None:
                     if isinstance(receptors, str):
-                        s = s.filter('term', **{'receptors.keyword': receptors})
-                    else:
-                        s = s.query('terms', **{'receptors': receptors})
+                        if receptors != '':
+                            s = s.filter('term', **{'receptors.keyword': receptors})
+                    elif receptors:
+                        s = s.query('terms', **{'receptors.keyword': receptors})
                 if query is not None and query != '':
                     s = s.query('multi_match', query=query, fields=['name', 'description'])
                 if sort_fields:
