d2de5d7fdb3c65a1edc9959ea2ab1927b6e016ab
antonino-tocco <entonytocco@gmail.com>
2021-06-07 21:20:26 +0200

    use internal electrophysiology provider

--- helpers/providers/__init__.py
+++ helpers/providers/__init__.py
@@ -5,5 +5,5 @@ from .hippocampome import HippocampomeProvider
 from .internal_morphology import InternalMorphologyProvider
 from .internal_electrophysiology import InternalElectrophysiologyProvider
 
-enabled_dataset_providers = [NeuroMorphoProvider(), InternalMorphologyProvider(), HippocampomeProvider()]
+enabled_dataset_providers = [NeuroMorphoProvider(), InternalMorphologyProvider(), InternalElectrophysiologyProvider(), HippocampomeProvider()]
 enabled_model_providers = [ModelDbProvider()]
