0849a9e256b4abdd46c84e28493f5c9d71b9f5f0
Antonino Tocco <entonytocco@gmail.com>
2023-05-20 11:56:17 +0200

    fix for ssl

--- helpers/create_connector.py
+++ helpers/create_connector.py
@@ -4,7 +4,7 @@ import certifi
 
 def create_connector():
     ssl_context = ssl.create_default_context(cafile=certifi.where())
-    conn = aiohttp.TCPConnector(ssl=ssl_context, verify_ssl=False)
+    conn = aiohttp.TCPConnector(ssl=ssl_context)
     return conn
 
 
